d3d02b59c010ea5bce957b9f5ee0eea573e9616d
Mike Teuten <mike.teuten@gmail.com>
2023-02-07 00:20:42 +0000

    Tidy up api key - use variables to construct api key

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-  //let appiKey1 = e1f97b83ade972f3c39403efd7d09291;
+ 
+
+
+
+  var api = "http://api.openweathermap.org/geo/1.0/direct?q="
+
   var APIKey = "91985d9f8baeb6a32bdc3cc0b3a89be8";
 
   //url: request city to retrieve the data for.
@@ -17,10 +22,7 @@
 
   function fetchForecast() {
     fetch(
-      "http://api.openweathermap.org/geo/1.0/direct?q=" +
-        city +
-        "&appid=" +
-        APIKey
+      api + city + "&appid=" + APIKey
     )
       .then((response) => response.json())
       .then((citiesFound) => {
@@ -42,23 +44,35 @@
         // show data on to html
   
         data.list.map((currentTemp, idx) => {
+
+          let section = document.getElementById("today");
+          let firstCity = document.createElement("h3");
+          firstCity.innerText = city;
           // console.log(currentTemp)
           // shows the first day, and the next days that are intervals of 8
           if (idx === 0 || idx % 8 == 0) {
+
+            // Add date to top of card
+
+            
+
+
             let icon = currentTemp.weather[0].icon;
             let humidity = currentTemp.main.humidity;
             let windSpeed = currentTemp.wind.speed;
+
   
             let section = document.getElementById("forecast");
             let firstCity = document.createElement("p");
             firstCity.innerText = city;
-  
+            
             //making the list
-            let ul = document.createElement("ul");
+            let ul = document.createElement("ul",);
             //making the list item
             let liIcon = document.createElement("li");
             //Setting the text value of the list item
             liIcon.innerText = icon;
+
   
             let liHumidity = document.createElement("li");
             liHumidity.innerText = humidity;
@@ -73,7 +87,7 @@
             ul.append(liWindSpeed)
   
             // we add the city and the ul to the section
-            section.append(firstCity)
+            //section.append(firstCity)
             section.append(ul)
           }
         });
